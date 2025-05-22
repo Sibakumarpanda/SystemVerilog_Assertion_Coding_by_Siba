ee09e4d22f39e1daae6a5f27c7d2b929572988be
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-05-22 14:59:48 +0530

    Create Q60_testbench.sv and Committed Solution for qstn60

---
+++ Q60_testbench.sv
@@ -0,0 +1,9 @@
+//Q60. What is a sequence repetition operator? What are the three different type of repetition operators used in sequences in SVA?
+
+In SystemVerilog Assertions (SVA), sequence repetition operators are used to specify how many times a particular event or condition should occur within a sequence.
+These operators allow you to define complex temporal behaviors by repeating certain parts of a sequence a specified number of times. 
+There are three different types of repetition operators used in sequences in SVA:
+   
+  1. Consecutive Repetition ([*n ])
+  2. Non-Consecutive Repetition ([=n])
+  3. Goto Repetition ([->n])
