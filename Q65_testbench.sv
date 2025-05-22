a787cbeca1f7f61e1320db6c8b89dbc81e4c768c
Siba Kumar Panda <51659486+Sibakumarpanda@users.noreply.github.com>
2025-05-22 22:30:07 +0530

    Create Q65_testbench.sv and Committed Solution for qstn65

---
+++ Q65_testbench.sv
@@ -0,0 +1,6 @@
+/*Q65 Are following assertions equivalent?
+ a) @(posedge clk) req |=> ##10 $rose(ack);
+ b) @(posedge clk) req |-> ##11 $rose(ack); 
+ */
+
+Refer Question No 26 and its Solution for this (NOTE: Question No 26 and Question No 65 , both are same)
